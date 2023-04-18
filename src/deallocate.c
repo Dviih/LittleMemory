1e663842921a0732ba46b03d2bc210395d25c4aa
Dviih <dviih2s@gmail.com>
2023-04-18 18:28:27 -0300

    Let's deallocate memory.

---
+++ src/deallocate.c
@@ -0,0 +1,9 @@
+#include "littlememory.h"
+
+void deallocate(void* ptr) {
+	chunk_t* chunk = (chunk_t*) (ptr - sizeof(chunk_t));
+
+	if (chunk->next == 0 || chunk->prev) return;
+	chunk->prev->next = chunk->next;
+	chunk->next->prev = chunk->prev;
+}
